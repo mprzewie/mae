acba22dd16f5272928ddcb82e26781d17071584f
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-05-24 09:02:17 +0200

    fix

--- models_mae.py
+++ models_mae.py
@@ -18,7 +18,7 @@ import torch.nn as nn
 from timm.models.vision_transformer import PatchEmbed, Block
 
 from util.pos_embed import get_2d_sincos_pos_embed
-
+from models_vit import Block
 
 class MaskedAutoencoderViT(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
