3e4dd20404775a31e47c80300f16daa23b0369c0
mprzewie <m.przewie@gmail.com>
2024-06-13 17:35:37 +0200

    add vit tiny

--- models_vit.py
+++ models_vit.py
@@ -203,6 +203,12 @@ class Attention(nn.Module):
         return x, attn
 
 
+def vit_tiny_patch16(**kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 def vit_base_patch16(**kwargs):
     model = VisionTransformer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
