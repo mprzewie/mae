34a007fa7d769915084f19be571473b17461e8ac
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-07-16 00:14:29 +0200

    fix

--- models_vit.py
+++ models_vit.py
@@ -209,9 +209,9 @@ def vit_tiny_patch16(img_size=224, patch_size=16, **kwargs):
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
-def vit_base_patch16(**kwargs):
+def vit_base_patch16(img_size=224, patch_size=16, **kwargs):
     model = VisionTransformer(
-        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        img_size=img_size, patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
