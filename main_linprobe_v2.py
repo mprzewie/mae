d31c110c36023a2285ccfc7a13450cd57f65a50d
mprzewie <m.przewie@gmail.com>
2024-09-04 19:07:31 +0200

    attn_only

--- main_linprobe_v2.py
+++ main_linprobe_v2.py
@@ -134,7 +134,7 @@ def get_args_parser():
     parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://',
                         help='url used to set up distributed training')
-    parser.add_argument("attn_only", action="store_true", default=False)
+    parser.add_argument("--attn_only", action="store_true", default=False)
 
 
     return parser
