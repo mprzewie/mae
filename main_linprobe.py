64d65b4b42dddb332c13b6dcabe63a1abe13b955
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-10-07 11:08:12 +0200

    sgd

--- main_linprobe.py
+++ main_linprobe.py
@@ -82,7 +82,7 @@ def get_args_parser():
     parser.add_argument("--checkpoint_key", default="model", type=str)
 
     # Dataset parameters
-    parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
+    parser.add_argument('--data_path', default=Path('/datasets01/imagenet_full_size/061417/'), type=Path,
                         help='dataset path')
     parser.add_argument('--nb_classes', default=1000, type=int,
                         help='number of the classification types')
